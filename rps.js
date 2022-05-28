a12c093c0112399e40ea92dfb5665a4e2d1556f3
t0mmylam <tommylam1004@gmail.com>
2022-05-28 17:01:50 -0400

    Finished all logic and added score

--- rps.js
+++ rps.js
@@ -4,7 +4,7 @@ function getRandomInt(max) {
 
 function computerPlay() {
     let choice;
-    switch(getRandomInt(3)) {
+    switch (getRandomInt(3)) {
         case 0:
             choice = "Rock";
             break;
@@ -15,35 +15,78 @@ function computerPlay() {
             choice = "Scissors";
             break;
     }
-    console.log(choice);
-    return(choice.toLowerCase());
+    return (choice.toLowerCase());
 }
 
 function playerPlay() {
     let choice = prompt("Type Rock, Paper, or Scissors.")
-    return(choice.toLowerCase());
+    return (choice.toLowerCase());
 }
 
-function game(){
-    let pScore = 0;
-    let cScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let result = round(playerPlay(), computerPlay());
-        console.log(result);
-        if (result.substring(0, 8) == "You win!") {
-            pScore+=1;
-        }
-        else if (result.substring(0, 9) == "You lose.") {
-            cScore+=1;
+function round(player, computer) {
+    if (player == computer) {
+        return ("The game is a tie.");
+    }
+    else if (player == "scissors") {
+        if (computer == "rock") {
+            return ("You lose. Rock crushes scissors.");
         }
         else {
-            pScore+=0.5;
-            cScore+=0.5;
+            return ("You win! Scissors cuts paper!");
         }
-        console.log("PSCORE: "+pScore);
-        console.log("CSCORE: "+cScore);
     }
-    console.log("You had "+pScore+" ponts and the computer had "+cScore+" points.");
+    else if (player == "rock") {
+        if (computer == "paper") {
+            return ("You lose. Paper covers rock.");
+        }
+        else {
+            return ("You win! Rock crushes scissors!");
+        }
+    }
+    else {
+        if (computer == "rock") {
+            return ("You win! Paper covers rock!");
+        }
+        else {
+            return ("You lose. Scissors cuts paper.");
+        }
+    }
 }
 
-game();
+function changeScore(results, pScore, cScore) {
+    console.log(pScore, cScore);
+    if (results.substring(0, 8) == "You win!") {
+        pScore += 1;
+    }
+    else if (results.substring(0, 9) == "You lose.") {
+        cScore += 1;
+    }
+    else {
+        pScore += 0.5;
+        cScore += 0.5;
+    }
+    return [pScore, cScore];
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const scoreDiv = document.querySelector('#score');
+const scores = document.createElement('div');
+scores.textContent = "Player Score: " + playerScore + " | Computer Score: " + computerScore;
+scoreDiv.appendChild(scores);
+
+const buttons = document.querySelectorAll('button');
+const results = document.createElement('div');
+const container = document.querySelector('.results');
+container.appendChild(results);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let temp = round(button.id, computerPlay());
+        results.textContent = temp;
+        let tempScores = changeScore(temp, playerScore, computerScore);
+        playerScore = tempScores[0];
+        computerScore = tempScores[1];
+        scores.textContent = "Player Score: " + playerScore + " | Computer Score: " + computerScore;
+    })
+});
